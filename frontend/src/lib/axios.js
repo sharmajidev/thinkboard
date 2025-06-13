74c23d7e6cca2b055dcbc6014f1c1207e2798089
sharmajidev <amansharma7949@gmail.com>
2025-06-13 23:31:03 +0530

    initial commit

---
+++ frontend/src/lib/axios.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api";
+
+const api = axios.create({
+  baseURL: BASE_URL,
+});
+
+export default api;
